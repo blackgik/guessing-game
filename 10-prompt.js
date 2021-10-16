bf6e71cce3caba1decae20fe93d9e66a8a35af00
echendu Chinedum <echendu.chinedum@gmail.com>
2021-10-16 12:01:47 +0100

    initial commit

--- 10-prompt.js
+++ 10-prompt.js
@@ -1,43 +1,47 @@
-const { stdin } = require('process');
-const readline = require('readline');
+const { RSA_X931_PADDING } = require("constants");
+const { stdin } = require("process");
+const readline = require("readline");
 
 // creating the stdin and stdout
 const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-})
-const rangeOfNumbers = [0,1,2,3,4,5,6,7,8,9,10]
-let num = Math.floor((Math.random()) * rangeOfNumbers.length );
-
-let count = 0
-let output =[]
-
-// using while to check for tries
-while (count < 3) {
-    // asking the question
-    rl.question('guess a number from 1-10:\n' , (userInput) => {
-
-        if (userInput === num){
-            console.log({
-                numberChoosenRandomly: num,
-                numberChoosenByUser: userInput
-            })
-            let numberOftries = output.length
-            const s = numberOftries > 1 ? 'tries' : 'try'
-            console.log('nice you guessed right, it took you ' + correctAnswerIndex, s)
-
-            rl.close()
-        } else {
-            console.log('you entered \'' + userInput + '\' The number was ' + num )
-            output.push(userInput)
-            
-            count += 1;
-        }
-        
-    });
-}
-
-
-rl.on('close', ()=> {
-    console.log('congrats!!!!')
+  input: process.stdin,
+  output: process.stdout,
 });
+
+const rangeOfNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let count = 0;
+let output = [];
+
+const guessingGame = () => {
+  let num = Math.floor(Math.random() * rangeOfNumbers.length);
+  rl.question("guess a number from 1-10: ", (dataInput) => {
+    if (output.length === rangeOfNumbers.length) {
+        console.log("#############################################")
+      console.log({ error: "exceed the amount the tries" });
+
+      rl.close();
+    }
+
+    dataInput = typeof dataInput === "string" ? Number(dataInput) : false;
+    if (dataInput && dataInput !== num) {
+      output.push(dataInput);
+      console.log(output);
+      console.log({ error: "incorrect number", realNumer: num });
+
+      guessingGame();
+    } else if (dataInput && dataInput === num) {
+      output.push(dataInput);
+
+      console.log(`it took you ${output.length} time(s) to get the number\n`);
+      console.log({ randomNumber: num, yourNumber: dataInput });
+
+      rl.close();
+    }
+  });
+};
+
+while (count <= rangeOfNumbers.length) {
+  guessingGame();
+
+  count += 1;
+}
